7092fab3fbb508b34b0699428b0ae416a83dfb50
Willie-McCaleb <wmccaleb13@gmail.com>
2023-05-09 01:38:47 -0500

    changes

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://universe-v24c.onrender.com/api/",
   withCredentials: true,
 });
 
